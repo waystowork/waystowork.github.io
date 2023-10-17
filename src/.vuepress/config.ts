460b4fb961678a1cc89906e275c0bdadb6ca10e3
数字生活指南 <61579343+waystowork@users.noreply.github.com>
2023-10-17 11:34:13 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -11,23 +10,6 @@ export default defineUserConfig({
 
   theme,
   
-  plugins: [
-      searchProPlugin({
-        // 索引全部内容
-        indexContent: true,
-        // 为分类和标签添加索引
-        customFields: [
-          {
-            getter: (page) => page.frontmatter.category,
-            formatter: "分类：$content",
-          },
-          {
-            getter: (page) => page.frontmatter.tag,
-            formatter: "标签：$content",
-          },
-        ],
-      }),
-    ],
 
   // Enable it with pwa
   // shouldPrefetch: false,
