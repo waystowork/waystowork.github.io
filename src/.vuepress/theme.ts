0dba5a0c040a498df550f1412e61641725ba3ca9
waystowork <zp0539@yeah.net>
2023-10-21 20:57:07 +0800

    更新 theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "https://waystowork.github.io",
 
   author: {
     name: "数字生活指南",
